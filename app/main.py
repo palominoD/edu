7ecfe7413a6cc856ea5c724622057b10d24a14b7
ebuntu <ed@ebuntu>
2024-08-26 17:53:44 -0600

    comenzando la estructura del servidr htmlcon flask

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from  flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.router('/')
+def home():
+    return render_template('index.html')
+
+@app.router('/index.html')
+def index():
+    return render_template('index.html')
