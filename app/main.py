495089f473eff94b74fe7d5b0bb6ad4845768bcf
ebuntu <ed@ebuntu>
2024-09-04 12:37:41 -0600

    HTML funcionando, completo en flask

--- app/main.py
+++ app/main.py
@@ -2,14 +2,22 @@ from  flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.router('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.router('/index.html')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+@app.route('/meetings.html')
+def meetings():
+    return render_template('meetings.html')
+
+@app.route('/meeting-details.html')
+def meeting():
+    return render_template('meeting-details.html')
+
 if __name__ == '__main__':
-app.run(host = '0.0.0.0', port = 1010)
+    app.run(host = '0.0.0.0', port = 1010)
 
